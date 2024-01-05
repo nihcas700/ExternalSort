346b6053dcc76db71dbc98dc3f656f69577e85f5
Sachin Tolay <stolay@splunk.com>
2024-01-04 19:07:04 -0800

    Optimization 6

--- src/main/java/diskbased/blockingio/DivideAndScatterJob.java
+++ src/main/java/diskbased/blockingio/DivideAndScatterJob.java
@@ -6,6 +6,8 @@ import org.apache.logging.log4j.Logger;
 import utils.Utils;
 
 import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
@@ -30,14 +32,22 @@ public class DivideAndScatterJob {
 
     public void divideAndScatter(String intermediateFilePath, String inputFilePath) throws IOException {
         long start = System.currentTimeMillis();
-        BufferedReader reader = new BufferedReader(new FileReader(inputFilePath), inputChunkSize);
-
-        int counter = 1, fileNo = 1;
-        List<Integer> list = new ArrayList<>(inputChunkSize);
+//        BufferedReader reader = new BufferedReader(new FileReader(inputFilePath), inputChunkSize);
+        RandomAccessFile file = new RandomAccessFile(inputFilePath, "rw");
+        FileChannel channel = file.getChannel();
+        ByteBuffer buffer = ByteBuffer.allocate(2048);
+        long pos = 0;
+        int fileNo = 1, currNo = 0;
+        List<Integer> list = new ArrayList<>();
         Map<String, ThreadMetadata> threadMetadata = new LinkedHashMap<>();
         while (true) {
-            String line = reader.readLine();
-            if (counter % inputChunkSize == 0 || line == null) {
+//            String line = reader.readLine();
+            int bytesRead = channel.read(buffer, pos);
+            buffer.flip();
+            currNo = convertToInt(buffer, currNo, list);
+            pos += bytesRead;
+            buffer.clear();
+            if ((!list.isEmpty() && (list.size() > inputChunkSize)) || bytesRead <= 0) {
                 String fileName = getIntermediateFileName(fileNo, 0);
                 BufferedWriter writer = new BufferedWriter(new FileWriter(Paths.get(intermediateFilePath, fileName).toString()), outputBufferSize);
 
@@ -48,14 +58,27 @@ public class DivideAndScatterJob {
                 metadata.setFuture(getSortAndFlushFuture(writer, list, metadata));
                 list = new ArrayList<>(inputChunkSize + 1);
                 fileNo++;
-                if (line == null) break;
+                if (bytesRead <= 0) break;
             }
-            list.add(Integer.parseInt(line));
-            counter++;
         }
         Utils.waitForThreadsToComplete(threadMetadata, -1, log);
         long end = System.currentTimeMillis();
         log.info("[divideAndSort] Divide and sort took " + (end - start) + " millis");
+    }
+
+    private int convertToInt(ByteBuffer data, int initial,
+                             List<Integer> list) {
+        int currNo = initial;
+        while (data.hasRemaining()) {
+            char ch = (char) (data.get() & 0xFF);
+            if (ch == '\n') {
+                list.add(currNo);
+                currNo = 0;
+            } else {
+                currNo = (currNo * 10) + (ch - '0');
+            }
+        }
+        return currNo;
     }
 
     private CompletableFuture<Object> getSortAndFlushFuture(BufferedWriter writer, List<Integer> list, ThreadMetadata metadata) {
